e45abab01c2c2b1d053af1a6410d5ad6fa2e0bfb
ctghadiri <ctghadiri@gmail.com>
2019-06-04 14:10:48 -0700

    define middleware, add routes, and comment next tasks

--- server.js
+++ server.js
@@ -6,3 +6,14 @@ const routes = require("./routes");
 const app = express()
 
 const PORT = process.env.PORT || 3001
+
+// define middleware
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// add routes, both API and view
+app.use(routes);
+
+// connect to MongoDB
+
+// start the API server
