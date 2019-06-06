af54ad3559619016c29400343cbe1c2447344465
ctghadiri <ctghadiri@gmail.com>
2019-06-06 11:55:28 -0700

    create email schema and export

--- models/email.js
+++ models/email.js
@@ -1,2 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
+const emailSchema = new Schema ({
+    firstName:{type: String, required: true},
+    lastName:{type: String},
+    company:{type: String},
+    email:{type: String},
+    subject:{type: String, required: true},
+    body:{type: String, required: true}
+});
+
+const Email = mongoose.model("Email", emailSchema);
+
+module.exports = Email;
