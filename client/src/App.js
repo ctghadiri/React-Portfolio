60b44cef3075d530a6b81fcac09dc4952499acbd
ctghadiri <ctghadiri@gmail.com>
2019-06-10 23:11:02 -0700

    create pages folder with main and contact and initial code to export/ add switch to app.js including pages main and contact

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Main from './pages/main';
+import Contact from './pages/contact';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
+  <Router>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,7 +23,11 @@ function App() {
         </a>
       </header>
     </div>
-  );
+    <Switch>
+      <Route exact path="/" component={Main} />  
+      <Route exact path- "/contact" component={Contact} />
+    </Switch>
+    </Router>
 }
 
 export default App;
