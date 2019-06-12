8b6cacddde7d8aece75dda6aa9fe6b38bf40b0f6
ctghadiri <ctghadiri@gmail.com>
2019-06-12 15:06:37 -0700

    delete app.css/app.test/index.css/logo.svg and prefilled code for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,32 +2,18 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Main from './pages/main';
 import Contact from './pages/contact';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  <Router>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-    <Switch>
-      <Route exact path="/" component={Main} />  
-      <Route exact path- "/contact" component={Contact} />
-    </Switch>
-    </Router>
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path- "/contact" component={Contact} />
+        </Switch>
+      </Router>
+    )
+  }
 }
 
 export default App;
